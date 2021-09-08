d6c9cc34a7d489d682e629058cab5b7e0fc33a8b
david <davidventura27@gmail.com>
2021-09-08 15:12:12 +0200

    update build scripts

--- whack_armv7.sh
+++ whack_armv7.sh
@@ -4,8 +4,8 @@ tmp=$(mktemp -d)
 here=$(pwd)
 version=$(grep version Cargo.toml | head -1 | cut -d'"' -f 2)
 wheel_path=$here/target/wheels/pykeepass_rs-$version-cp35-cp35m-manylinux_2_17_armv7l.manylinux2014_armv7l.whl
-unzip "$wheel_path" -d $tmp
+unzip -q "$wheel_path" -d $tmp
 cd $tmp
 mv pykeepass_rs/pykeepass_rs.cpython-35m-x86_64-linux-gnu.so pykeepass_rs/pykeepass_rs.cpython-35m-arm-linux-gnueabihf.so
 sudo rm -f "$wheel_path"
-sudo zip -r "$wheel_path" .
+sudo zip -qr "$wheel_path" .
