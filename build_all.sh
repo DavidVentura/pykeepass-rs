d549c255c25a23d64d1a5c8caf74ba42bd0056fd
DavidVentura <davidventura27@gmail.com>
2022-12-28 22:24:20 +0100

    also release on py3.8

--- build_all.sh
+++ build_all.sh
@@ -12,7 +12,9 @@ bash whack_armv7.sh
 echo 'Building AArch64'
 (cd aarch64 && docker build -t cross-aarch64 .)
 docker run -v ~/git/pykeepass-rs:/io --workdir /io cross-aarch64 build --release --strip -i python3.5 --target aarch64-unknown-linux-gnu --manylinux 2014
+docker run -v ~/git/pykeepass-rs:/io --workdir /io cross-aarch64 build --release --strip -i python3.8 --target aarch64-unknown-linux-gnu --manylinux 2014
 
 echo 'Building x86'
 docker build -t x86 -f Dockerfile_x86_64 .
 docker run --env RUSTFLAGS='-C target-cpu=ivybridge' -v ~/git/pykeepass-rs:/io x86 build --release --strip -i python3.5 --target x86_64-unknown-linux-musl --manylinux 2014
+docker run --env RUSTFLAGS='-C target-cpu=ivybridge' -v ~/git/pykeepass-rs:/io x86 build --release --strip -i python3.8 --target x86_64-unknown-linux-musl --manylinux 2014
